4bbda77058f8cb29ac483e9b58ae076a7462507d
Eugene Maslenkov <maslenkov@i.ua>
2018-04-27 17:40:13 +0300

    fix: db connection issue

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,8 +2,9 @@ module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
+      if (err) {
+        console.log(err);
+        res.send({ 'error': 'An error has occurred' });
       } else {
         res.send(result.ops[0]);
       }
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const db             = require('./config/db');
+const db_cred        = require('./config/db');
 const app            = express();
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(db_cred.url, (err, database) => {
   if (err) return console.log(err)
-  db = database.db("landingdb")
+  db = database.db("landing_project")
   require('./app/routes')(app, db);
   app.listen(port, () => {
     console.log('We are live on ' + port);
-  });               
+  });
 })
