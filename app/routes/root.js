9403b01cde13721d75c561dc8bc4315123b37352
AlexiyV <alevaliev@yandex.ru>
2018-05-02 21:43:26 +0300

    landing page project
    what is done:
    -added users data noinput check
    -added some new ejs files
    
    what should be done:
    -must make styles path working
    -must added check for input rools errors

--- app/routes/root.js
+++ app/routes/root.js
@@ -2,7 +2,10 @@ module.exports = function(app, res) {
   app.get('/', (req, res) => {
     const path = require('path');
     res.render('default', {
-      title : 'Landing test index'
+      title : 'Landing test index',
+      headerclass   : 'header'  ,
+      contentclass  : 'content' ,
+      footerclass   : 'footer'
     });
   });
 };
--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,18 +1,40 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
   
+  function CorrectInputCheck(login, password, email, group) {
+    if (login && password && email && group) {
+      return true;
+    } else {
+      var inerr = '';
+      var errtext = 'incorrect';
+      if (!login) {
+        inerr = inerr + ' ' + errtext + ' login,'
+      }
+      if (!password) {
+        inerr = inerr + ' ' + errtext + ' password,';
+      } 
+      if (!email) {
+        inerr = inerr + ' ' + errtext + ' email,';
+      } 
+      if (!group) {
+        inerr = inerr + ' ' + errtext + ' group,';
+      } 
+      return({'error': inerr.substring(1,inerr.length-1)});
+    }
+  }
+
   app.post('/users', (req, res) => {
     const user = {login: req.body.login, password: req.body.password, email: req.body.email, group: req.body.groups};
-    db.collection('users').insert(user, (err, result) => {
-      if (err) {
-        console.log(err);
-        res.send({ 'error': 'An error has occurred' });
-      } else {
-        if (req.body.login) {
-          res.send(result.ops[0]);
-        } else res.send({'error':'no correct login'});
-      }
-    });
+    if (CorrectInputCheck(req.body.login,req.body.password,req.body.email,req.body.groups) == true) {
+      db.collection('users').insert(user, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.send({ 'error': 'An error has occurred' });
+        } else {
+          res.send(result.ops[0]);  
+        }
+      });
+    } else res.send(CorrectInputCheck(req.body.login,req.body.password,req.body.email,req.body.groups));
   });
 
   app.get('/users/:id', (req, res) => {
@@ -44,14 +66,16 @@ module.exports = function(app, db) {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
     const user = {login: req.body.login, password: req.body.password, email: req.body.email, group: req.body.groups};
-    db.collection('users').update(details, user, (err, result) => {
-      if (err) {
-          res.send({'error':'An error has occurred'});
-      } else {
-          res.send(user);
-          console.log(req.body.login);
-        } 
-    });
+    if (CorrectInputCheck(req.body.login,req.body.password,req.body.email,req.body.groups) == true) {
+      db.collection('users').update(details, user, (err, result) => {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            res.send(user);
+            console.log(req.body.login);
+          } 
+      });
+    } else res.send(CorrectInputCheck(req.body.login,req.body.password,req.body.email,req.body.groups));
   });
 };
 
